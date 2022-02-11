5f75557c12129a77f1dade8819ccd64376397f94
Dave Burke <dburke84@gmail.com>
2022-02-10 20:43:13 -0600

    Add initial etherpad container definition

--- lib/cdk-workadventure-stack.ts
+++ lib/cdk-workadventure-stack.ts
@@ -1,16 +1,24 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecs from 'aws-cdk-lib/aws-ecs'
 
 export class CdkWorkadventureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const etherpadTask = new ecs.FargateTaskDefinition(this, 'EtherpadTask', {
+      memoryLimitMiB: 256,
+      cpu: 128,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkWorkadventureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    etherpadTask.addContainer('etherpad', {
+      image: ecs.ContainerImage.fromRegistry('etherpad/etherpad:1.8.6'),
+      environment: {
+        'TITLE': process.env.ETHERPAD_TITLE || 'Video Chat',
+        'DEFAULT_PAD_TEXT': process.env.ETHERPAD_DEFAULT_PAD_TEXT || 'Welcome to Web Chat!\n\n',
+        'SKIN_NAME': process.env.ETHERPAD_SKIN_NAME || 'colibris',
+        'SKIN_VARIANTS': process.env.ETHERPAD_SKIN_VARIANTS || 'super-light-toolbar super-light-editor light-background full-width-editor',
+      },
+    });
   }
 }
