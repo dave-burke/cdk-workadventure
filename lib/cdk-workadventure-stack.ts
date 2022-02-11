ccee5a75fdf625fd95b707a147e4961f11ee803f
Dave Burke <dburke84@gmail.com>
2022-02-10 20:23:05 -0600

    Initial container definitions

--- lib/cdk-workadventure-stack.ts
+++ lib/cdk-workadventure-stack.ts
@@ -1,16 +1,39 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
 
 export class CdkWorkadventureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkWorkadventureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const jitsiTask = new ecs.FargateTaskDefinition(this, 'JitsiTask', {
+      memoryLimitMiB: 2048,
+      cpu: 1024,
+    });
+    const webContainer = jitsiTask.addContainer("web", {
+      image: ecs.ContainerImage.fromRegistry("jitsi/web"),
+      portMappings: [
+        { containerPort: 80 },
+        { containerPort: 443 },
+      ],
+    });
+    const prosodyContainer = jitsiTask.addContainer("prosody", {
+      image: ecs.ContainerImage.fromRegistry("jitsi/prosody"),
+      portMappings: [
+        { containerPort: 5222 },
+        { containerPort: 5347 },
+        { containerPort: 5280 },
+      ],
+    });
+    const jicofoContainer = jitsiTask.addContainer("jicofo", {
+        image: ecs.ContainerImage.fromRegistry("jitsi/jicofo"),
+    });
+    const jvbContainer = jitsiTask.addContainer("jvb", {
+      image: ecs.ContainerImage.fromRegistry("jitsi/jvb:latest"),
+      portMappings: [
+        { containerPort: 10000, protocol: ecs.Protocol.UDP },
+        { containerPort: 4443 },
+      ],
+    });
   }
 }
